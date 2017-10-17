b96b6128377acfb63130c01ccb066ba5ceb98c42
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2017-10-17 07:09:11 +0200

    Update demo

---
+++ config.js
@@ -0,0 +1,11 @@
+window.CONFIG = {
+  TITLE: "React Auth Page demo",
+  SUBTITLE: "Sign in to start your session",
+  USER_CREDENTIALS_ENABLED: true,
+  OAUTH_AUTH_URL: "https://react-auth-page.eu.auth0.com/auth",
+  OAUTH_TOKEN_URL: "https://react-auth-page.eu.auth0.com/oauth/token",
+  OAUTH_REDIRECT_URL: "http://localhost:3000/secret-page.html",
+  OAUTH_CLIENT_ID: "7c5zatls1niu0SJv6Qr6c9GwCD0oxemO",
+  OAUTH_SCOPE: "email",
+  OAUTH_AUDIENCE: "https://react-auth-page.eu.auth0.com/userinfo"
+};
